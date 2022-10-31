092e7732fdd28a0896cbd212f11568908b432052
splotchysnow <46405838+splotchysnow@users.noreply.github.com>
2022-10-30 18:54:43 -0700

    init

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,27 @@
 // expose.js
 
+/**
+ * Notes: Input events:
+ * Change: When user modifies the elements value
+ * Load : When an image loaded.
+ * Click : After mousedown, mouseup. Pressed and released!
+ */
+
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  //Grabbing all nessisary element from HTML and assigning them to a variable.
+  let intrument_selector = document.querySelector("#horn-select");
+  let volume_selector = document.querySelector("#volume");
+  let button_selector = document.querySelector("button")
+
+  //Event listeners:
+  volume_selector.addEventListener("change",function(e){})
+
+  console.log(slider_selector);
+}
+
+
+function update_value(){
+  //NOTHING FOR NOW.
 }
